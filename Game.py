47b8ff574c2905230203796b3e677e1f87735634
LuisDalcico <luisdalcico@outlook.com>
2025-11-09 19:28:02 -0300

    até aqui certo

--- Game.py
+++ Game.py
@@ -1,1 +1,21 @@
 #arquivo do jogo
+import pygame
+import sys
+import random
+# Inicializando o Pygame
+pygame.init()
+
+# Configurando a tela
+largura_tela = 800
+altura_tela = 600
+tela = pygame.display.set_mode((largura_tela, altura_tela))
+pygame.display.set_caption('Flappy Bird')
+
+# Relógio do jogo
+relogio = pygame.time.Clock()
+
+# Carregando imagens e fundo
+personagem = pygame.image.load('personagem.jpg')
+# aumentar o personagem 3x
+pw, ph = personagem.get_size()
+personagem = pygame.transform.scale(personagem, (pw * 3, ph * 3))
